d48309cfb6c8953b908a0409390a2e94ca7b4842
youwonsock <qazwsx233434@gmail.com>
2023-11-05 02:25:46 +0900

    set base project setting

---
+++ DirectX Team Portfolio/Engine/Types.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXCollision.h>
+
+#ifndef XM_CONSTEXPR
+#define XM_CONSTEXPR
+#endif
+
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+using Vector2 = XMFLOAT2;
+using Vector3 = XMFLOAT3;
+using Vector4 = XMFLOAT4;
+using Color = XMFLOAT4;
---
+++ Library/include/Engine/ExLib.h
@@ -0,0 +1,37 @@
+#pragma once
+
+// direct x header
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <dxgi1_2.h>
+
+// direct 2d header
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+
+//for ComPtr
+#include <wrl.h>
+
+// additional Lib header
+#include "FMOD/fmod.h"
+#include "FMOD/fmod.hpp"
+#include <FBXSDK/fbxsdk.h>
+
+// link direct x library
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dcompiler.lib")
+#pragma comment (lib, "dxgi.lib")
+
+// link direct 2d library
+#pragma comment (lib, "d2d1.lib")
+#pragma comment (lib, "dwrite.lib")
+
+// link additional Lib library
+#pragma comment (lib, "fmod_vc.lib")
+#pragma comment(lib, "libfbxsdk-md.lib")
+#pragma comment(lib, "libxml2-md.lib")
+#pragma comment(lib, "zlib-md.lib")
+
+using namespace Microsoft::WRL;					// for ComPtr
+
